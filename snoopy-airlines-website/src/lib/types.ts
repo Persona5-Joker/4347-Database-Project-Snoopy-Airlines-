63c2ac08f4ac746e8bd839f2cd5cff0d581551b9
Fernando4242 <portillofernando92@gmail.com>
2024-11-11 18:15:02 -0600

    simple types for reservation request

---
+++ snoopy-airlines-website/src/lib/types.ts
@@ -0,0 +1,12 @@
+export interface CreateReservationRequest {
+  outboundFlightId: string;
+  returnFlightId?: string;
+  passengers: Passenger;
+}
+
+export interface Passenger {
+  firstName: string;
+  lastName: string;
+  dob: string;
+  baggageWeights: number[];
+}
